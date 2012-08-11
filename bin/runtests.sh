1a949839ec2fbf77e9b8c70e71c41ff9d49064b5
Brian Scaturro <scaturrob@gmail.com>
2012-08-10 20:46:54 -0400

    run tests anywhere

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-phpunit --bootstrap ../src/bootstrap.php --color ../src/Test/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SRC="$DIR/../src/"
+TEST="$SRC/Test/"
+phpunit --bootstrap "$SRC"bootstrap.php --color "$TEST"
