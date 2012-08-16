28fa548159cdc9895dda7da2c7ad44a2c8fa93fd
Brian Scaturro <scaturrob@gmail.com>
2012-08-15 22:05:42 -0400

    add twig view engine

--- src/Presentation/index.php
+++ src/Presentation/index.php
@@ -1,2 +1,10 @@
 <?php
 require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+$app = new Slim(array(
+    'view' => 'TwigView'
+));
+
+$app->get('/register', function() use($app) {
+    $app->render('register.php');
+});
