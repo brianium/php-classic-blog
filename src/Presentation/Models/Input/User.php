e1101996bf8f2f8194d16719ac061b55d219262c
Brian Scaturro <scaturrob@gmail.com>
2012-08-16 23:32:19 -0400

    added input model tests

--- src/Presentation/Models/Input/User.php
+++ src/Presentation/Models/Input/User.php
@@ -5,27 +5,54 @@ class User
 {
     protected $data;
     protected $validator;
+    protected $messages;
 
-    public function __construct($data) {
+    public function __construct($data, $msgs = []) {
         $this->data = $data;
         $this->validator = new Base();
+        $this->messages = $msgs;
     }
 
     public function isValid()
     {
+        $this->initValidation();
+
+        $success = $this->validator->execute($this->data);
+
+        $this->applyMessages();
+
+        return $success;
+    }
+
+    protected function initValidation()
+    {
         $this->validator->validate('username', function($v){
-            return $v->require();
+            return $v->require()
+                and $v->atMostChars(50);
         })->validate('password', function($v){
             return $v->require();
         })->validate('passwordConfirm', function($v){
             return $v->require();
         });
-
-        return $this->validator->execute($this->data);
     }
 
-    public function getMessage($key)
+    public function applyMessages()
     {
-        return 'string';
+        foreach($this->messages as $key => $msg) {
+            $parts = explode('.', $key);
+            $value = $this->validator->getError($parts[0])->getValue();
+            if($value)
+                $value->getValidation()->setMessage($parts[1], $msg);
+        }
+    }
+
+    public function __get($key)
+    {
+        return @$this->data[$key];
+    }
+
+    public function getMessageFor($key)
+    {
+        return $this->validator->getErrorMessage($key);
     }
 }
--- src/Test/Unit/Presentation/Models/Input/UserTest.php
+++ src/Test/Unit/Presentation/Models/Input/UserTest.php
@@ -5,16 +5,17 @@ use Presentation\Models\Input\User;
 class UserTest extends TestBase
 {
     protected $data;
+    protected $input;
 
     public function setUp()
     {
         $this->data = ['username' => 'brian', 'password' => 'pass', 'passwordConfirm' => 'pass'];
+        $this->input = new User($this->data);
     }
 
     public function test_isValid_should_return_false_if_username_omitted()
     {
-        $this->data['username'] = null;
-        $input = new User($this->data);
+        $input = $this->getInput(['username' => null]);
 
         $valid = $input->isValid();
 
@@ -23,8 +24,7 @@ class UserTest extends TestBase
 
     public function test_isValid_should_return_false_if_password_omitted()
     {
-        $this->data['password'] = null;
-        $input = new User($this->data);
+        $input = $this->getInput(['password' => null]);
 
         $valid = $input->isValid();
 
@@ -33,8 +33,7 @@ class UserTest extends TestBase
 
     public function test_isValid_should_return_false_if_passwordConfirm_omitted()
     {
-        $this->data['passwordConfirm'] = null;
-        $input = new User($this->data);
+        $input = $this->getInput(['passwordConfirm' => null]);
 
         $valid = $input->isValid();
 
@@ -43,20 +42,90 @@ class UserTest extends TestBase
 
     public function test_isValid_should_return_true_if_all_fields_present()
     {
-        $input = new User($this->data);
-
-        $valid = $input->isValid();
+        $valid = $this->input->isValid();
 
         $this->assertTrue($valid);
     }
 
+    public function test_isValid_should_return_false_if_username_greather_than_50_chars()
+    {
+        $this->setLongUsername();
+        $input = new User($this->data);
+
+        $valid = $input->isValid();
+
+        $this->assertFalse($valid);
+    }
+
+    public function test_unknown_property_is_empty()
+    {
+        $this->assertEmpty($this->input->sandwich);
+    }
+
+    public function test_known_property_is_returned()
+    {
+        $this->assertEquals('brian', $this->input->username);
+    }
+
     public function test_should_have_error_message_for_username_if_username_is_empty()
     {
-        $this->data['username'] = null;
-        $input = new User($this->data);
+        $input = $this->getInput(['username' => null]);
 
         $input->isValid();
 
-        $this->assertNotEmpty($input->getMessage('username'));
+        $this->assertNotEmpty($input->getMessageFor('username'));
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function test_error_message_not_set_before_isValid_called()
+    {
+        $input = $this->getInput(['username' => null]);
+
+        $this->assertEmpty($input->getMessageFor('username'));
+    }
+
+    public function test_getMessage_returns_empty_for_unkown_property()
+    {
+        $this->input->isValid();
+
+        $this->assertEmpty($this->input->getMessageFor('hamsandwich'));
+    }
+
+    public function test_constructed_with_error_array_sets_message_by_key_when_one()
+    {
+        $msg = 'Please provide a username'; 
+        $input = $this->getInput(['username' => null], ['username.nonEmpty' => $msg]);
+
+        $input->isValid();
+
+        $this->assertEquals($msg, $input->getMessageFor('username'));
+    }
+
+    public function test_constructed_with_error_array_sets_message_by_key_when_two()
+    {
+        $msgNonEmpty = 'Please provide a username'; 
+        $msgAtMost = 'Username cannot be longer than 50 chars';
+        $this->setLongUsername(); 
+        $input = $this->getInput([], ['username.nonEmpty' => $msgNonEmpty, 'username.atMostChars' => $msgAtMost]);
+
+        $input->isValid();
+
+        $this->assertEquals($msgAtMost, $input->getMessageFor('username'));
+    }
+
+    public function setLongUsername()
+    {
+        $username = '';
+        for($i = 1; $i <= 51; $i++)
+            $username .= 'a';
+        $this->data['username'] = $username;
+    }
+
+    protected function getInput($vals, $msgs = [])
+    {
+        $this->data = array_merge($this->data, $vals);
+        return new User($this->data, $msgs);
     }
 }
