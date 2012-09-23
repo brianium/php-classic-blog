d3d5a86c6f2d2163f36984b39038a54b74721e4a
Brian Scaturro <scaturrob@gmail.com>
2012-09-22 22:01:47 -0400

    Presentation: SlimAuthenticationService complete

--- src/Presentation/Services/SlimAuthenticationService.php
+++ src/Presentation/Services/SlimAuthenticationService.php
@@ -23,11 +23,39 @@ class SlimAuthenticationService
     public function isAuthenticated($cookiename)
     {
         if(empty($this->routes)) return true;
-        foreach($this->routes as $route) {
-            if('/' . $route == $this->slim->request()->getPath()) {
-                $cookie = $this->slim->getCookie('cookiename');
-                if(is_null($cookie)) return false;
-            }
-        }
+
+        $route = array_reduce($this->routes, array($this, 'matchesCurrentRoute'), '/.*/');
+
+        return $this->authCookieIsValid($cookiename);
+    }
+
+    public function matchesCurrentRoute($route)
+    {
+        $pattern = $route[0] == '/';
+        if(!$pattern) $route = '/^\/' . $route . '$/';
+        return (bool)preg_match($route, $this->slim->request()->getPath());
+    }
+
+    public function authCookieIsValid($cookiename)
+    {
+        list($identifier, $token) = $this->splitAuthCookie($cookiename);
+
+        if(!$identifier) return false;
+
+        $users = $this->userRepo->getBy(['identifier' => $identifier]);
+        if(!$users) return false;
+
+        return !$this->hasExpired($users[0], $token);
+    }
+
+    protected function hasExpired($user, $token)
+    {
+        return $token != $user->getToken() || time() > $user->getTimeout();
+    }
+
+    protected function splitAuthCookie($cookiename) {
+        $cookie = $this->slim->getCookie($cookiename);
+        if(is_null($cookie)) return [false, false];
+        return explode(':', $cookie);
     }
 }
--- src/Test/Unit/Presentation/Services/SlimAuthenticationServiceTest.php
+++ src/Test/Unit/Presentation/Services/SlimAuthenticationServiceTest.php
@@ -9,10 +9,13 @@ class SlimAuthenticationServiceTest extends TestBase
     protected $request;
 
     protected $userRepo;
+    protected $user;
+
     protected $service;
 
     public function setUp()
     {
+        parent::setUp();
         $this->slim = $this->getMockBuilder('Slim')
                            ->disableOriginalConstructor()
                            ->getMock();
@@ -20,12 +23,17 @@ class SlimAuthenticationServiceTest extends TestBase
         $this->request = $this->getMockBuilder('Slim_Http_Request')
                               ->disableOriginalConstructor()
                               ->getMock();
+
+        $this->request->expects($this->any())
+                      ->method('getPath')
+                      ->will($this->returnValue('/admin'));
         
         $this->slim->expects($this->any())
                    ->method('request')
                    ->will($this->returnValue($this->request));
 
         $this->userRepo = $this->getMock('Domain\\Repositories\\UserRepository');
+        $this->user = $this->loadFixture('Test\\Fixtures\\User\\UserNoPosts', 'Domain\\Entities\\User');
 
         $this->service = new SlimAuthenticationService($this->slim, $this->userRepo);
         $this->service->addRoute('admin');
@@ -61,15 +69,90 @@ class SlimAuthenticationServiceTest extends TestBase
 
     public function test_isAuthenticated_returns_false_for_null_cookie_on_current_path()
     {
-        $this->request->expects($this->once())
-                      ->method('getPath')
-                      ->will($this->returnValue('/admin'));
+        $this->cookieReturnsNull();
+        $this->assertFalse($this->service->isAuthenticated('cookiename'));
+    }
 
+    public function test_isAuthenticated_returns_false_when_token_invalid()
+    {
+        $this->cookieReturnsInvalidToken();
+        $this->userRepoReturnsUserFixture();
+        $this->assertFalse($this->service->isAuthenticated('cookiename'));               
+    }
+
+    public function test_isAuthenticated_returns_false_when_no_users_found()
+    {
+        $this->cookieReturnsValidCookie();
+        $this->userRepoReturnsEmptyArray();
+        $this->assertFalse($this->service->isAuthenticated('cookiename'));
+    }
+
+    public function test_isAuthenticated_returns_false_when_now_greater_than_user_timeout()
+    {
+        $this->cookieReturnsValidCookie();
+        $this->user->setTimeout(strtotime("-1 week"));
+        $this->userRepoReturnsUserFixture();
+        $this->assertFalse($this->service->isAuthenticated('cookiename'));
+    }
+
+    public function test_isAuthenticated_returns_true_when_valid_token_and_timeout()
+    {
+        $this->cookieReturnsValidCookie();
+        $this->user->setTimeout(strtotime("+1 week"));
+        $this->userRepoReturnsUserFixture();
+        $this->assertTrue($this->service->isAuthenticated('cookiename'));
+    }
+
+    public function test_matchesCurrentRoute_should_match_pattern()
+    {
+        $match = $this->service->matchesCurrentRoute('/^\/ad.*/');
+        $this->assertTrue($match);
+    }
+
+    protected function cookieReturnsInvalidIdentifier()
+    {
+        $this->slim->expects($this->once())
+                   ->method('getCookie')
+                   ->with($this->identicalTo('cookiename'))
+                   ->will($this->returnValue('notfound' . ':' . $this->user->getToken()));
+    }
+
+    protected function userRepoReturnsEmptyArray()
+    {
+        $this->userRepo->expects($this->once())
+                       ->method('getBy')
+                       ->will($this->returnValue([]));
+    }
+
+    protected function userRepoReturnsUserFixture()
+    {
+        $this->userRepo->expects($this->once())
+                       ->method('getBy')
+                       ->with($this->identicalTo(['identifier' => $this->user->getIdentifier()]))
+                       ->will($this->returnValue([$this->user]));
+    }
+
+    protected function cookieReturnsInvalidToken($value='')
+    {
+        $this->slim->expects($this->once())
+                   ->method('getCookie')
+                   ->with($this->identicalTo('cookiename'))
+                   ->will($this->returnValue($this->user->getIdentifier() . ':' . 'invalidtoken'));
+    }
+
+    protected function cookieReturnsNull()
+    {
         $this->slim->expects($this->once())
                    ->method('getCookie')
                    ->with($this->identicalTo('cookiename'))
                    ->will($this->returnValue(null));
+    }
 
-        $this->assertFalse($this->service->isAuthenticated('cookiename'));
+    protected function cookieReturnsValidCookie()
+    {
+        $this->slim->expects($this->once())
+                   ->method('getCookie')
+                   ->with($this->identicalTo('cookiename'))
+                   ->will($this->returnValue($this->user->getTokenString()));
     }
 }
